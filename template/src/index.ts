da650763e1d09d957897aea0be55f3b4bfcc77b4
doriongilmore <doriongilmore@outlook.com>
2025-08-15 18:32:05 +0200

    fix: template/src/index.ts should only run one file by default

--- template/src/index.ts
+++ template/src/index.ts
@@ -2,8 +2,8 @@ import { resolve } from "path"
 import { runPuzzle } from "@doriongilmore/codingame-ts-engine";
 import script from "./playerScript.js";
 
-// const oneFile = resolve("exercises", "01.json");
-// runPuzzle(script, oneFile);
+const oneFile = resolve("exercises", "01.json");
+runPuzzle(script, oneFile);
 
-const folder = resolve("exercises");
-runPuzzle(script, folder);
+// const folder = resolve("exercises");
+// runPuzzle(script, folder);
